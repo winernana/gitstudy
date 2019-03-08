aec3704d8340dbbcb55a8181ac74e6c7bf5cf078
winer <495893694@qq.com>
2019-03-08 09:20:18 +0800

    爱鲜蜂首页详细信息

--- pythonaxf/app/models.py
+++ pythonaxf/app/models.py
@@ -1,3 +1,80 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    img=models.CharField(max_length=100)
+    name=models.CharField(max_length=100)
+    trackid=models.CharField(max_length=20)
+
+    class Meta:
+        # 抽象类
+        abstract = True
+
+# 轮播图 模型类
+class Wheel(BaseModel):
+    class Meta:
+        db_table = 'axf_wheel'
+
+# 导航 模型类
+class Nav(BaseModel):
+    class Meta:
+        db_table = 'axf_nav'
+
+# 每日必购
+class Mustbuy(BaseModel):
+    class Meta:
+        db_table='axf_mustbuy'
+
+
+#部分商品
+class Shop(BaseModel):
+    class Meta:
+        db_table='axf_shop'
+
+# 商品列表，模型类
+class Mainshow(BaseModel):
+    trackid = models.CharField(max_length=100)
+    name=models.CharField(max_length=100)
+    img=models.CharField(max_length=100)
+    categoryid=models.CharField(max_length=20)
+    brandname=models.CharField(max_length=100)
+
+    img1=models.CharField(max_length=100)
+    childcid1=models.CharField(max_length=100)
+    productid1=models.CharField(max_length=20)
+    longname1=models.CharField(max_length=100)
+    price1=models.CharField(max_length=20)
+    marketprice1=models.CharField(max_length=100)
+
+    img2 = models.CharField(max_length=100)
+    childcid2 = models.CharField(max_length=100)
+    productid2 = models.CharField(max_length=20)
+    longname2 = models.CharField(max_length=100)
+    price2 = models.CharField(max_length=20)
+    marketprice2 = models.CharField(max_length=100)
+
+    img3 = models.CharField(max_length=100)
+    childcid3 = models.CharField(max_length=100)
+    productid3 = models.CharField(max_length=20)
+    longname3 = models.CharField(max_length=100)
+    price3 = models.CharField(max_length=20)
+    marketprice3 = models.CharField(max_length=100)
+
+    class Meta:
+        db_table='axf_mainshow'
+
+    def __str__(self):
+        return self.name
+
+# insert into axf_foodtypes(typeid,typename,childtypenames,typesort)
+class Foodtype(models.Model):
+    # 分类id
+    typeid=models.CharField(max_length=20)
+    # 分类名称
+    typename=models.CharField(max_length=100)
+    # 子类（多个）
+    childtypenames=models.CharField(max_length=255)
+    # 分类排序
+    typesort=models.IntegerField()
+
+    class Meta:
+        db_table='axf_foodtypes'
--- pythonaxf/app/views.py
+++ pythonaxf/app/views.py
@@ -1,12 +1,51 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app.models import Wheel, Nav, Mustbuy, Shop, Mainshow, Foodtype
+
+
 def home(request):
-    return render(request,'home/home.html')
+    # 轮播图数据
+    wheels=Wheel.objects.all()
+
+    #获取导航
+    navs = Nav.objects.all()
+
+    #每日必购
+    mustbuys = Mustbuy.objects.all()
+
+    #商品部分
+    shops = Shop.objects.all()
+
+    shead = shops[0]
+    stab = shops[1:3]
+    sclass = shops[3:7]
+    scommend = shops[7:]
+
+    # 商品列表
+    mainshows=Mainshow.objects.all()
+
+    data={
+        'wheels':wheels,
+        'navs':navs,
+        'mustbuys':mustbuys,
+        'shead':shead,
+        'stab':stab,
+        'sclass':sclass,
+        'scommend':scommend,
+        'mainshows':mainshows,
+    }
+
+    return render(request,'home/home.html',context=data)
 
 
 def market(request):
-    return render(request,'market/market.html')
+    foodtypes = Foodtype.objects.all()
+
+    data1={
+        'foodtypes':foodtypes,
+    }
+    return render(request,'market/market.html',context=data1)
 
 
 def cart(request):
